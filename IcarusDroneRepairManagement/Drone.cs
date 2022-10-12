234fc6405063ca1b4d2fc316d5c55d7204829406
patrickwheatley2004 <patrickwheatley2004@gmail.com>
2022-10-12 10:14:59 +0800

    Add project files.

---
+++ IcarusDroneRepairManagement/Drone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcarusDroneRepairManagement
+{
+    internal class Drone
+    {
+        private string clientName;
+        private string droneModel;
+        private string serviceProblem;
+        private double serviceCost;
+        private string serviceTag;
+    }
+}
